c9b95184b2fa53f3462359f134e029bff348004f
leonardo <leonardofranciscocustodio@gmail.com>
2023-10-11 21:33:34 -0400

    CRUD add

---
+++ application/dao/ProdutoDAO.php
@@ -0,0 +1,16 @@
+<?php
+
+class ProdutoDAO{
+    // CRUD
+    // Create (C)
+    public function salvar($produto){}
+    public function pegaTodos(){}
+    // Retrieve (R)
+    public function pegaPorId($id){}
+    // Update (U)
+    public function atualizar($produto){}
+    // Delete (D)
+    public function apagar($id){}
+}
+
+?>
