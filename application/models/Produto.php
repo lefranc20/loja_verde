9594e95b155ddd9a4c2fb779c29376b6f4f8040b
leonardo <leonardofranciscocustodio@gmail.com>
2023-10-11 21:25:55 -0400

    Criei o model do Produto

---
+++ application/models/Produto.php
@@ -0,0 +1,45 @@
+<?php
+
+class Produto{
+    private $codigo;
+    private $nome;
+    private $marca;
+    private $preco;
+    
+    public function __construct($nome, $marca, $preco){
+        $this->nome = $nome;
+        $this->marca = $marca;
+        $this->preco = $preco;
+    }
+
+    public function setCodigo($codigo){
+        $this->$codigo = $codigo;
+    }
+    public function getCodigo(){
+        return $this->codigo;
+    }
+
+    public function setNome($nome){
+        $this->$nome = $nome;
+    }
+    public function getNome(){
+        return $this->nome;
+    }
+
+    public function setMarca($marca){
+        $this->$marca = $marca;
+    }
+    public function getMarca(){
+        return $this->marca;
+    }
+
+    public function setPreco($preco){
+        $this->$preco = $preco;
+    }
+    public function getPreco(){
+        return $this->preco;
+    }
+
+}
+
+?>
